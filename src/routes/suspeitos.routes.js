e04c2603206d56f638ba975eb1d9c58f3560810a
gabriela-moleta <gabriela.moleta@aluno.senai.br>
2024-10-10 15:43:24 -0300

    feat: finalização das rotas

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const suspeitosRoutes = Router();
 
-let candidatos = [
+let suspeito = [
     {
       id: 1,
       nome: "P. Diddy",
@@ -27,4 +27,43 @@ let candidatos = [
         nivelSuspeita: "medio"
         }
     ];
+
+ //Rota para listar todos os suspeitos
+suspeitosRoutes.get("/", (req, res) => {
+    return res.status(200).json(suspeitos);
+  });
+
+// Rota para buscar um suspeito pelo id
+suspeitosRoutes.get("/:id", (req, res) => {
+    const { id } = req.params;
+  
+    // Busca um suspeito pelo id no array de suspeitos
+    const suspeito = suspeitos.find((perseguido) => perseguido.id == id);
+  
+    // Verifica se o suspeito foi encontrado
+    if (!suspeito) {
+      return res
+        .status(404)
+        .json({ message: `suspeito com id ${id} não encontrado!` });
+    }
+  
+    return res.status(200).json(suspeito);
+  });
+  
+
+  // Rota para atualizar um suspeito pelo id
+suspeitosRoutes.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { nome, profissao, envolvimento, nivelSuspeita } = req.body;
+});
+  
+  // Remove o suspeito do array de suspeito
+  suspeitos = suspeitos.filter((suspeito) => suspeito.id != id);
+
+  return res.status(200).json({
+    message: "Suspeito removido com sucesso!",
+    suspeito,
+  });
+
+export default suspeitosRoutes;
 

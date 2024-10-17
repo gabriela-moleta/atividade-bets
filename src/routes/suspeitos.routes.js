3ec2f2a39f4c92838c17afc965669a7cde06afaf
gabriela-moleta <gabriela.moleta@aluno.senai.br>
2024-10-17 08:07:21 -0300

    feat: criação do get

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -7,63 +7,30 @@ let suspeito = [
       id: 1,
       nome: "P. Diddy",
       profissao: "Cantor",
-      envolvimento: true,
-      nivelSuspeita: "alto"
+      envolvimento: sim,
+      nivelSuspeita: "alto",
     },
 
     {
         id: 2,
         nome: "Jay-Z",
         profissao: "Cantor",
-        envolvimento: true,
-        nivelSuspeita: "alto"
+        envolvimento: sim,
+        nivelSuspeita: "alto",
     },
 
     {
         id: 3,
         nome: "Beyonce",
         profissao: "Cantor",
-        envolvimento: true,
-        nivelSuspeita: "medio"
+        envolvimento: sim,
+        nivelSuspeita: "medio",
         }
     ];
 
- //Rota para listar todos os suspeitos
-suspeitosRoutes.get("/", (req, res) => {
-    return res.status(200).json(suspeitos);
-  });
-
-// Rota para buscar um suspeito pelo id
-suspeitosRoutes.get("/:id", (req, res) => {
-    const { id } = req.params;
-  
-    // Busca um suspeito pelo id no array de suspeitos
-    const suspeito = suspeitos.find((perseguido) => perseguido.id == id);
-  
-    // Verifica se o suspeito foi encontrado
-    if (!suspeito) {
-      return res
-        .status(404)
-        .json({ message: `suspeito com id ${id} não encontrado!` });
-    }
-  
-    return res.status(200).json(suspeito);
+    suspeitosRoutes.get("/", (req, res) => {
+      return res.status(200).send(suspeitos);
   });
   
-
-  // Rota para atualizar um suspeito pelo id
-suspeitosRoutes.put("/:id", (req, res) => {
-    const { id } = req.params;
-    const { nome, profissao, envolvimento, nivelSuspeita } = req.body;
-});
   
-  // Remove o suspeito do array de suspeito
-  suspeitos = suspeitos.filter((suspeito) => suspeito.id != id);
-
-  return res.status(200).json({
-    message: "Suspeito removido com sucesso!",
-    suspeito,
-  });
-
-export default suspeitosRoutes;
-
+  export default suspeitosRoutes;

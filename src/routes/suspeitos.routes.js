dd1eee93dac88376066f009f9a13aad4b3b20e43
gabriela-moleta <gabriela.moleta@aluno.senai.br>
2024-10-17 08:12:44 -0300

    feat: criação do get para puxar o id

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -28,26 +28,28 @@ let suspeito = [
         }
     ];
 
-    suspeitosRoutes.get("/", (req, res) => {
-      return res.status(200).send(suspeitos);
+    suspeitosRoutes.post("/", (req, res) => {
+      const { nome, profissão, envolvimento, nivelSuspeita } = req.body;
+      if (!nome || !profissão || !nivelSuspeita) {
+          return res.status(400).send({ message: "Nome, Profissão e Nível de Suspeita são Obrigatórios" });
+      }
+      const novoSuspeito = {
+          id: suspeitos.length + 1,
+          nome,
+          profissão,
+          envolvimento,
+          nivelSuspeita
+      };
+      suspeitos.push(novoSuspeito);
+      return res.status(201).send(novoSuspeito);
   });
   
-  suspeitosRoutes.post("/", (req, res) => {
-    const { nome, profissão, envolvimento, nivelSuspeita } = req.body;
-    if (!nome || !profissão || !nivelSuspeita) {
-        return res.status(400).send({ message: "Nome, Profissão e Nível de Suspeita são Obrigatórios" });
-    }
-    const novoSuspeito = {
-        id: suspeitos.length + 1,
-        nome,
-        profissão,
-        envolvimento,
-        nivelSuspeita
-    };
-  })
-
-  suspeitos.push(novoSuspeito);
-      return res.status(201).send(novoSuspeito);
+  suspeitosRoutes.get("/:id", (req, res) => {
+      const { id } = req.params;
+      const suspeito = suspeitos.find((suspeito) => suspeito.id == id);
+      if (!suspeito) {
+          return res.status(404).send({ message: `Suspeito com id ${id} não foi encontrado!` });
+      }
+      return res.status(200).json(suspeito);
+  });
   
-  
-  export default suspeitosRoutes;
